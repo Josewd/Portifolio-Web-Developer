775cc6b85731333ae8272f79bfda2354c1cfe2f9
josewd <exageradonome@gmail.com>
2022-03-15 16:39:50 +0000

    first commit: adding react app, base fonts from google fonts.

---
+++ portifolio-developer/src/App.js
@@ -0,0 +1,16 @@
+import classes from './App.module.scss';
+
+function App() {
+  return (
+    <>
+    <div className={classes.Container}>
+      <div className={classes.Title}>Jose Barbosa Silva</div>
+      <div className={classes.Subtitle}>FRONTEND WEB DEVELOPER</div>
+    </div>
+    <div className={classes.Container}>
+    </div>
+    </>
+  );
+}
+
+export default App;
